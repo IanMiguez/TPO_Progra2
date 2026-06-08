6dd695469084bfd6c3c86342ad7c8e26a8e5409b
Ian <ianfrancomiguez@gmail.com>
2026-06-08 00:15:21 -0300

    Clase Perfil

--- src/Perfil.java
+++ src/Perfil.java
@@ -2,30 +2,40 @@ public class Perfil {
 
     private int id;
     private String nombre;
-    private String puesto;
-    private String empresa;
+    private String profesion;
 
-    public Perfil(int id, String nombre, String puesto, String empresa) {
+    public Perfil(int id, String nombre, String profesion) {
         this.id = id;
         this.nombre = nombre;
-        this.puesto = puesto;
-        this.empresa = empresa;
+        this.profesion = profesion;
     }
 
-    public int getId() { return id; }
-    public String getNombre() { return nombre; }
-    public String getPuesto() { return puesto; }
-    public String getEmpresa() { return empresa; }
+    public int getId() {
+        return id;
+    }
 
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public void setPuesto(String puesto) { this.puesto = puesto; }
-    public void setEmpresa(String empresa) { this.empresa = empresa; }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
 
     @Override
     public String toString() {
-        return "[ID: " + id
-                + " | Nombre: " + nombre
-                + " | Puesto: " + puesto
-                + " | Empresa: " + empresa + "]";
+        return "Perfil{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", profesion='" + profesion + '\'' +
+                '}';
     }
 }

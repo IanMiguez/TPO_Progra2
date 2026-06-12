71e014d729e7448e5ea80a91185d8223c38dbd1d
Ian <ianfrancomiguez@gmail.com>
2026-06-12 15:35:32 -0300

    Metodo Constructor y regitrarUsuario

--- src/RedProfesional.java
+++ src/RedProfesional.java
@@ -1,2 +1,16 @@
 public class RedProfesional {
+
+    private Diccionario<Integer, Perfil> usuarios;
+
+    public RedProfesional() {
+        usuarios = new Diccionario<>();
+    }
+
+    public boolean registrarUsuario(Perfil perfil) {
+
+        return usuarios.insertar(
+                perfil.getId(),
+                perfil
+        );
+    }
 }

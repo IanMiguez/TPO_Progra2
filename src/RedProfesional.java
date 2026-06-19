3219d256e1f014eb3ba3baa9bff087dad93d5542
Ian <ianfrancomiguez@gmail.com>
2026-06-19 14:57:01 -0300

    Integración de Postulacion a RedProfesional

--- src/RedProfesional.java
+++ src/RedProfesional.java
@@ -1,31 +1,43 @@
 public class RedProfesional {
-
     private Diccionario<Integer, Perfil> usuarios;
+    private ColaCircular<Postulacion> postulaciones;
 
     public RedProfesional() {
         usuarios = new Diccionario<>();
+        postulaciones = new ColaCircular<>(100);
     }
 
+    // Usuarios
     public boolean registrarUsuario(Perfil perfil) {
-
-        return usuarios.insertar(
-                perfil.getId(),
-                perfil
-        );
+        return usuarios.insertar(perfil.getId(), perfil);
     }
 
     public Perfil buscarUsuario(int id) {
-
         return usuarios.buscar(id);
     }
 
     public boolean eliminarUsuario(int id) {
-
         return usuarios.eliminar(id);
     }
 
     public void mostrarUsuarios() {
-
         usuarios.mostrar();
+    }
+
+    // Postulaciones
+    public boolean agregarPostulacion(Postulacion postulacion) {
+        return postulaciones.encolar(postulacion);
+    }
+
+    public Postulacion procesarPostulacion() {
+        return postulaciones.desencolar();
+    }
+
+    public Postulacion verSiguientePostulacion() {
+        return postulaciones.verFrente();
+    }
+
+    public int cantidadPostulaciones() {
+        return postulaciones.tamaño();
     }
 }
